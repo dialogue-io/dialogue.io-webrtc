95a1b304bb4c4e37e49b2836758da98b92449f08
Albert Abello <albert.abello@ilustrum.com>
2012-12-03 16:15:08 +0200

    Adapted to new version of API, droped support for peerconnection00

--- node-login/app/public/js/controllers/webrtcController.js
+++ node-login/app/public/js/controllers/webrtcController.js
@@ -140,6 +140,7 @@ $(document).ready(function(){
     }
   });
 
+
   RTCchan = function(receiver,from,div){
     this.div = div;
     this.from = from;
@@ -216,9 +217,12 @@ $(document).ready(function(){
     
     //When incomming message from websocket this function will process it into the PC
     this.onChannelMessage = function(message) {
-       console.log('S->C: ' + message);
-       if (isRTCPeerConnection) processSignalingMessage(message);
-       else processSignalingMessage00(message);
+       log = JSON.parse(message);
+       if (log.sdp != undefined)
+       console.log('S->C: ' +log.type+'\n'+ log.sdp);
+       else
+       console.log('S->C: '+ JSON.stringify(message));
+       processSignalingMessage(message);
     }
 
     function initialize() {
@@ -338,37 +342,12 @@ $(document).ready(function(){
 
     function doCall() {
        console.log("Sending offer to peer.");
-       if (isRTCPeerConnection) {
-           //pc.createOffer(setLocalAndSendMessage, null, mediaConstraints);
-          pc.createOffer(setLocalAndSendMessage);
-       } else {
-           //var offer = pc.createOffer(mediaConstraints);
-           var offer = pc.createOffer();
-           pc.setLocalDescription(pc.SDP_OFFER, offer);
-           sendMessage({
-               type: 'offer',
-               sdp: offer.toSdp()
-           },receiver,from);
-           pc.startIce();
-       }
+      pc.createOffer(setLocalAndSendMessage);
     }
 
     function doAnswer() {
        console.log("Sending answer to peer.");
-       if (isRTCPeerConnection) {
-           //pc.createAnswer(setLocalAndSendMessage, null, mediaConstraints);
-           pc.createAnswer(setLocalAndSendMessage);
-       } else {
-           var offer = pc.remoteDescription;
-           //var answer = pc.createAnswer(offer.toSdp(), mediaConstraints);
-           var answer = pc.createAnswer(offer.toSdp());
-           pc.setLocalDescription(pc.SDP_ANSWER, answer);
-           sendMessage({
-               type: 'answer',
-               sdp: answer.toSdp()
-           },receiver,from);
-           pc.startIce();
-       }
+       pc.createAnswer(setLocalAndSendMessage);
     }
 
     setRemoteVideo = function(tag,callback) {
@@ -428,6 +407,9 @@ $(document).ready(function(){
     };
    
     function setLocalAndSendMessage(sessionDescription) {
+       //Prefer Opus if avaliable, good for performance comparisions? 
+       sessionDescription.sdp = preferOpus(sessionDescription.sdp);
+       //comment the above function to go back to VP8 
        pc.setLocalDescription(sessionDescription);
        sendMessage(sessionDescription,receiver,from);
     }
@@ -437,11 +419,7 @@ $(document).ready(function(){
        if (msg.type === 'offer') {
            // Callee creates PeerConnection
            if (!initiator && !started) maybeStart();
-
-           // We only know JSEP version after createPeerConnection().
-           if (isRTCPeerConnection) pc.setRemoteDescription(new RTCSessionDescription(msg));
-           else pc.setRemoteDescription(pc.SDP_OFFER, new SessionDescription(msg.sdp));
-
+           pc.setRemoteDescription(new RTCSessionDescription(msg));
            doAnswer();
        } else if (msg.type === 'answer' && started) {
            pc.setRemoteDescription(new RTCSessionDescription(msg));
@@ -451,20 +429,6 @@ $(document).ready(function(){
                candidate: msg.candidate
            });
            pc.addIceCandidate(candidate);
-       } else if (msg.type === 'bye' && started) {
-           onRemoteHangup();
-       }
-    }
-
-    function processSignalingMessage00(message) {
-       var msg = JSON.parse(message);
-
-       // if (msg.type === 'offer') should not happen here.
-       if (msg.type === 'answer' && started) {
-           pc.setRemoteDescription(pc.SDP_ANSWER, new SessionDescription(msg.sdp));
-       } else if (msg.type === 'candidate' && started) {
-           var candidate = new IceCandidate(msg.label, msg.candidate);
-           pc.processIceMessage(candidate);
        } else if (msg.type === 'bye' && started) {
            onRemoteHangup();
        }
@@ -642,6 +606,8 @@ $(document).ready(function(){
        sendMessage({
            type: 'bye'
        },receiver,from);
+            //Delay 100ms to ensure 'bye' arrives first.
+       setTimeout(function(){}, 100);
     }
 
     // Ctrl-D: toggle audio mute; Ctrl-E: toggle video mute.
@@ -667,6 +633,78 @@ $(document).ready(function(){
                return false;
            }
        }
+    }
+
+    //Opus part just in case we need to run performance tests
+    // Set Opus as the default audio codec if it's present.
+    function preferOpus(sdp) {
+      var sdpLines = sdp.split('\r\n');
+
+      // Search for m line.
+      for (var i = 0; i < sdpLines.length; i++) {
+          if (sdpLines[i].search('m=audio') !== -1) {
+            var mLineIndex = i;
+            break;
+          } 
+      }
+      if (mLineIndex === null)
+        return sdp;
+
+      // If Opus is available, set it as the default in m line.
+      for (var i = 0; i < sdpLines.length; i++) {
+        if (sdpLines[i].search('opus/48000') !== -1) {        
+          var opusPayload = extractSdp(sdpLines[i], /:(\d+) opus\/48000/i);
+          if (opusPayload)
+            sdpLines[mLineIndex] = setDefaultCodec(sdpLines[mLineIndex], opusPayload);
+          break;
+        }
+      }
+
+      // Remove CN in m line and sdp.
+      sdpLines = removeCN(sdpLines, mLineIndex);
+
+      sdp = sdpLines.join('\r\n');
+      return sdp;
+    }
+
+    function extractSdp(sdpLine, pattern) {
+      var result = sdpLine.match(pattern);
+      return (result && result.length == 2)? result[1]: null;
+    }
+
+    // Set the selected codec to the first in m line.
+    function setDefaultCodec(mLine, payload) {
+      var elements = mLine.split(' ');
+      var newLine = new Array();
+      var index = 0;
+      for (var i = 0; i < elements.length; i++) {
+        if (index === 3) // Format of media starts from the fourth.
+          newLine[index++] = payload; // Put target payload to the first.
+        if (elements[i] !== payload)
+          newLine[index++] = elements[i];
+      }
+      return newLine.join(' ');
+    }
+
+    // Strip CN from sdp before CN constraints is ready.
+    function removeCN(sdpLines, mLineIndex) {
+      var mLineElements = sdpLines[mLineIndex].split(' ');
+      // Scan from end for the convenience of removing an item.
+      for (var i = sdpLines.length-1; i >= 0; i--) {
+        var payload = extractSdp(sdpLines[i], /a=rtpmap:(\d+) CN\/\d+/i);
+        if (payload) {
+          var cnPos = mLineElements.indexOf(payload);
+          if (cnPos !== -1) {
+            // Remove CN payload from m line.
+            mLineElements.splice(cnPos, 1);
+          }
+          // Remove CN line in sdp
+          sdpLines.splice(i, 1);
+        }
+      }
+
+      sdpLines[mLineIndex] = mLineElements.join(' ');
+      return sdpLines;
     }
   }
 
